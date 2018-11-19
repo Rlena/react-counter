74970b9c9d01e804bac95b20a95b02a2931df98b
Rlena <elena1902881@gmail.com>
2018-11-19 17:39:06 +0300

    Redux

--- redux.js
+++ redux.js
@@ -1,7 +1,64 @@
 const redux = require('redux')
 
-// Reducer
+// объект, описывающий состояние всего приложения
+const initialState = {
+  counter: 0
+}
 
-// Store
+// по умолчанию state = initialState
+// Reducer - js-функция, описывает действия
+// reducer всегда возвращает новый state, обрабатывает Action
+// в reducer описали начальное состояние,
+// далее если попадает некоторый action с пом. ф-ции dispatch, будем проверять action.type
+// при выполнении условия модифицируем state, на основае предыдущего состояния
+// вернем новый state
+const reducer = (state = initialState, action) => {
 
-// Actions
+  if (action.type === 'ADD') {
+    return {
+      // counter переопределяем из предыдущего значения + 1
+      counter: state.counter + 1
+    }
+  }
+
+  if (action.type === 'SUB') {
+    return {
+      counter: state.counter - 1
+    }
+  }
+
+  if (action.type === 'ADD_NUMBER') {
+    return {
+      counter: state.counter + action.value
+    }
+  }
+
+  return state
+
+}
+
+// Store - место, где хранятся все данные
+// метод getState() харанит текущее состояние store
+// stоre передаем функцию (reducer), кот. изменяет store
+const store = redux.createStore(reducer)
+
+// подписка на изменения store
+// при любом изменении store, мы будем попадать в данную функцию и получать актуальный state
+store.subscribe(() => {
+  console.log('Subscribe', store.getState())
+})
+
+// Actions предназначен для изменения Store
+// type - обязательное поле, определяет какое именно действие мы совершаем
+const addCounter = {
+  type: 'ADD'
+}
+
+// в dispatch передаем action
+store.dispatch(addCounter)
+
+store.dispatch({ type: 'SUB'})
+
+// данный объект мы получаем в reducer как параметр action
+store.dispatch({ type: 'ADD_NUMBER', value: 10 })
+
