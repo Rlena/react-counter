d602bb4ed1f8b80e1b6ba4c4ba19623e18b1aa23
Rlena <elena1902881@gmail.com>
2018-11-19 18:11:38 +0300

    Add rootReducer

--- src/index.js
+++ src/index.js
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux'
+import rootReducer from './redux/rootReducer'
+import {Provider} from 'react-redux'
 
+// создаем store
+// в createStore необходимо передать reducer
+// создали в отдельной директории rootReducer
+const store = createStore(rootReducer)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// сообщаем приложению, что теперь мы работаем с redux
+// передаем в Provider store
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
